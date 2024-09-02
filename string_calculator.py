c7cca470e1c51aa2974d0ad9525307b592d9484d
Mukesh Jain <mukeshj@Mukeshs-MacBook-Pro.local>
2024-09-02 20:11:28 +0530

    Support different delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -2,10 +2,9 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
+        if numbers.startswith("//"):
+            delimiter, numbers = numbers[2:].split('\n', 1)
+            numbers = numbers.replace(delimiter, ',')
         numbers = numbers.replace('\n', ',')
         num_list = numbers.split(',')
         return sum(int(num) for num in num_list)
-
-
-
-    
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -20,3 +20,8 @@ def test_add_multiple_numbers():
 def test_add_with_newlines():
     calculator = StringCalculator()
     assert calculator.add("1\n2,3") == 6
+
+def test_add_with_custom_delimiter():
+    calculator = StringCalculator()
+    assert calculator.add("//;\n1;2") == 3
+
