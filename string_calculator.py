10ebb837237ad5e031ef7c0f2feb34367d282979
Mukesh Jain <mukeshj@Mukeshs-MacBook-Pro.local>
2024-09-02 20:01:10 +0530

    Handle single number case

--- string_calculator.py
+++ string_calculator.py
@@ -2,3 +2,6 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
+        return int(numbers)
+
+    
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -4,3 +4,7 @@ from string_calculator import StringCalculator
 def test_add_empty_string():
     calculator = StringCalculator()
     assert calculator.add("") == 0
+
+def test_add_single_number():
+    calculator = StringCalculator()
+    assert calculator.add("1") == 1
